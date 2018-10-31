ef5a3ed65ecb69ca323741902ed497ac0fee6267
Eudaemonal <eudaemonal@gmail.com>
2018-10-31 08:14:38 -0700

    Update README

--- 02_mmap/test01.c
+++ 02_mmap/test01.c
@@ -1,11 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <assert.h>
 
 
+size_t getFilesize(const char* filename) {
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
 
-int main(int argc, char *argv[]){
+
+int main(int argc, char** argv) {
+    size_t filesize = getFilesize(argv[1]);
     
-	return EXIT_SUCCESS;
+    //Open file
+    int fd = open(argv[1], O_RDONLY, 0);
+    assert(fd != -1);
+    
+    
+    void* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    assert(mmappedData != MAP_FAILED);
+    
+    //Write the mmapped data to stdout (= FD #1)
+    write(1, mmappedData, filesize);
+    
+    //Cleanup
+    int rc = munmap(mmappedData, filesize);
+    assert(rc == 0);
+    
+    
+    close(fd);
 }
---
+++ README.md
@@ -0,0 +1,22 @@
+# OS
+
+```01_inotify```  
+```02_mmap```  
+```03_signal```  
+```04_pipe```
+
+```05_socket (sync, async)```
+```06_epoll```  
+
+```fork```
+```multi-process```
+
+```lock```
+```semaphore```
+```condition_variable```
+
+
+
+## How to use
+
+
